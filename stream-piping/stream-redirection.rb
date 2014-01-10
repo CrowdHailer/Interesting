57f75452cca5bda68eb921f5c429d1d24f1dcdc3
Peter Saxton <peterhsaxton@gmail.com>
2014-01-10 11:57:15 +0000

    Changed to explicitly declaire standard input and output

--- stream-piping/stream-redirection.rb
+++ stream-piping/stream-redirection.rb
@@ -1,9 +1,12 @@
-puts "Enter some Names"
-name = gets.chomp
+# $stdout.puts Is equivalent to puts 
+# $stdin.gets Is equivalent to gets
+
+$stdout.puts "Enter some Names"
+name = $stdin.gets.chomp
 i = 1
 while name != ""
-	puts "#{name} "*i
+	$stdout.puts "#{name} "*i
 	i += 1
-	name = gets.chomp
+	name = $stdin.gets.chomp
 end
 puts "Finished Names"
